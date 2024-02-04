c9d7b6e8a933a0c98f03becc209e26f1ed10fd1d
3DUTEX <t3xdev@gmail.com>
2024-02-04 17:04:30 -0300

    pegar os dados do usuário logado para verificar reservas

--- src/pages/Reservations/index.jsx
+++ src/pages/Reservations/index.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable consistent-return */
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import axios from '../../services/axios';
 import { ReservationsSection } from './styled';
 
 export default function Reservations() {
+  const authReducer = useSelector((state) => state.authReducer);
   const location = useLocation();
   const navigate = useNavigate();
+  const [dataUser, setDataUser] = useState({});
 
   // Verifica se tem o id do veículo
   useEffect(() => {
@@ -15,6 +19,25 @@ export default function Reservations() {
     }
     if (!location.state.idVehicle) return navigate('/'); // se não tiver id do veículo
   }, [location]);
+
+  // Solicitando dados do usuário logado
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const { data } = await axios.get('/users', {
+          headers: {
+            Authorization: `Bearer ${authReducer.token}`, // passando tokendo do usuário logado
+          },
+        });
+        setDataUser(data); // setando dados do usuário
+      } catch (e) {
+        const { data } = e;
+        console.log(data);
+      }
+    }
+
+    getUser(); // chamando função
+  }, []);
 
   return (
     <ReservationsSection>
